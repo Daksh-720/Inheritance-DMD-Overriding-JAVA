a3df27cb257a6114e5085d6a31872a08000e42ca
Daksh Salvi <dakshsalvi59@gmail.com>
2026-06-05 18:47:38 +0530

    Add files via upload

---
+++ foreach.java
@@ -0,0 +1,10 @@
+public class foreach {
+    public static void main(String[] args){
+        String[] animals = {"Cat", "Dog", "Cow", "Lion", "Tiger"};
+        
+        for(String i : animals){
+            System.out.println(i);
+        }
+    }
+    
+}
